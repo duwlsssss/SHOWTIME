553c77ccac40044271bf1744a9b01e4d86486182
whwjdan <35955189+whwjdan@users.noreply.github.com>
2024-11-22 18:17:08 +0900

    [⚙️ Setting] 프로젝트를 위한 초기 모듈 설정 (#2)
    * setting: 코드 포맷팅 및 husky, lint-staged 설정 추가 (#1)
    
    * setting: 타입스크립트 기본 세팅 추가 (#1)
    
    * setting: sass, styled-components, 리덕스 및 툴킷 추가 (#1)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,31 @@
 ﻿module.exports = {
-    env: {
-        browser: true,
-        node: true,
-        es6: true,
-    },
-    parserOptions: {
-        ecmaVersion: 2022,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-        },
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:prettier/recommended',
-        'plugin:react/recommended'
-    ],
-    rules: {
-        'no-unused-vars': 'off',
-    },
+	env: {
+		browser: true,
+		node: true,
+		es6: true,
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 2022,
+		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true,
+		},
+		project: ['./tsconfig.eslint.json'],
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:prettier/recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	rules: {
+		'no-unused-vars': 'off',
+		'react/react-in-jsx-scope': 'off',
+	},
 };
