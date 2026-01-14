8796412752b29ac6973be26fc5f929f31bece9f7
duwlsssss <rladuwls0814@gmail.com>
2026-01-14 21:46:07 +0900

    fix: 모듈 나누기

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,17 @@ export default defineConfig({
 	define: {
 		'import.meta.env.VITE_DEBUG': JSON.stringify(true),
 	},
+	build: {
+		rollupOptions: {
+			output: {
+				// node_modules의 라이브러리들을 각각 별도의 파일로 쪼개는 설정
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						// 라이브러리 이름을 추출하여 chunk 이름으로 사용
+						return id.toString().split('node_modules/')[1].split('/')[0].toString();
+					}
+				},
+			},
+		},
+	},
 });
